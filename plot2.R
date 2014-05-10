b0949e21747369468a45a409b8ff96dadaff1750
Charles Albrecht <charles.albrecht@pearson.com>
2014-05-10 10:14:50 -0700

    Remove margin manipulation as not needed.

--- plot2.R
+++ plot2.R
@@ -3,12 +3,10 @@ source("getData.R")
 # Plot 2
 
 png(filename="plot2.png", width=480, height=480, bg="transparent")
-oldmar <- par()$mar
-par(mar=oldmar)
+
 with (power, 
       plot(DateTime, Global_active_power, 
            type="l", 
            xlab="", ylab="Global Active Power (kilowatts)")
       )
-par(mar=oldmar)
 dev.off()
